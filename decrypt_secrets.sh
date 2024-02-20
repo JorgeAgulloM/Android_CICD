5ab707017214622b7648a824aeaa9cbf52cc3fe8
Jorge1 <agullojorge@gmail.com>
2024-02-20 21:21:14 +0100

    Fixed

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -4,6 +4,6 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="MY_SECRETS_KEY" \
+gpg --quiet --batch --yes --decrypt --passphrase="$MY_SECRETS_KEY" \
 --output $HOME/secrets/secrets.properties secrets.properties.gpg
 # end
